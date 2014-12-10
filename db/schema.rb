44e82e1643b2b6e0d2c54f76f18119f33d5dad44
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-10 09:34:42 -0800

    add user email address validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210172204) do
+ActiveRecord::Schema.define(version: 20141210172322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141210172204) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validates" do
+    it "email address contains an @ symbol" do
+      user = User.new(email: "batexample.com")
+      expect(user.valid?).to eq false
+    end
+  end
 end
