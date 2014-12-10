83f64d7135d40a644af80fb9afada5866071aecb
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-10 09:43:51 -0800

    add another user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   describe "validates" do
+    let(:user) { User.new(email: "b@example.com") }
+
+    it "is valid with valid attributes" do
+      expect(user.valid?).to eq true
+    end
+
     it "email address contains an @ symbol" do
-      user = User.new(email: "batexample.com")
+      user.email = "batexample.com"
       expect(user.valid?).to eq false
     end
+
+      
   end
 end
