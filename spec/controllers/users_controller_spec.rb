d275c555e492c5e95f601a540d1e1913032a1f69
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-10 10:33:02 -0800

    dry up the tests with a let:

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,19 +9,24 @@ RSpec.describe UsersController, :type => :controller do
   end
 
   describe "POST 'create'" do
+    let(:request) { post :create, user: {email: "b@example.com"} }
     # creates a new user
     # redirects to homepage
     # signs the user in
     # if it doesn't save, render the new form
     it "adds a new user to the database" do
       count = User.all.count
-      post :create, user: {email: "b@example.com"}
+      request
       expect(User.all.count).to eq count + 1
     end
 
     it "redirects to homepage" do
-      post :create, user: {email: "b@example.com"}
+      request
       expect(response).to redirect_to root_path
+    end
+
+    it "assign the user id to session" do
+      request
     end
   end
 end
